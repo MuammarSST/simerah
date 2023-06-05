f1cc983c5e720260674aaa4a8fad00bb11e9adec
xmuammar@gmail.com <Mynameisasus>
2023-06-05 11:31:23 +0700

    perbaiki tahunan

--- view/login.php
+++ view/login.php
@@ -3,9 +3,16 @@
 <html lang="en">
 
 <?php
-  require_once('../view/header.php');
-?>
+require_once('../view/header.php');
 
+require('../proses/koneksi.php');
+
+$query_tahun = "SELECT * FROM tahun";
+$result_tahun = mysqli_query($koneksi, $query_tahun);
+if (!$result_tahun) {
+    die('Invalid query: ' . $mysqli->error);
+}
+?>
 <body class="">
   <div class="wrapper ">
 
@@ -40,20 +47,39 @@
                   <form method="POST" action="../proses/cek_login.php">
                     <div class="row">
 
-                      <div class="col-md-6">
+                      <div class="col-md-12">
                         <div class="form-group">
                           <label class="bmd-label-floating">Username</label>
-                          <input type="text" class="form-control" name="username">
+                          <input type="text" class="form-control" name="username" required>
                         </div>
                       </div>
-                      <div class="col-md-6">
+                      <div class="col-md-12">
                         <div class="form-group">
                           <label class="bmd-label-floating">Password</label>
-                          <input type="password" class="form-control" name="password">
+                          <input type="password" class="form-control" name="password" required>
                         </div>
                       </div>
                     </div>
-                    
+                  
+                    <div class="form-group">
+                    <label class="bmd-label-floating">Tahun :</label>
+                        <select id="id_tahun" name="id_tahun" class="card" required>
+                        <option disabled selected required>--- Silahkan Pilih Tahun ---</option>
+                            <?php
+                            $i = 0;
+                            while ($row = @mysqli_fetch_assoc($result_tahun)) {
+
+                                $id_tahun = $row['id_tahun'];
+                                $tahun = $row['tahun'];
+
+                            ?>
+                                <option value="<?php echo $id_tahun; ?>"><?php echo $tahun; ?></option>
+                            <?php
+                                $i++;
+                            } ?>
+                        </select>
+                    </div>
+                   
                     
                     </div>
                     <button type="submit" class="btn btn-danger pull-right"><i class="fa fa-send"></i> Login</button>
